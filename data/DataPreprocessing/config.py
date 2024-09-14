4f9183e35674b43fc16644a8815393f5c87ab5da
Kline <klinedylan46@gmail.com>
2024-09-13 20:53:26 -0400

    refactor: 001 move datasets and update config

--- data/DataPreprocessing/config.py
+++ data/DataPreprocessing/config.py
@@ -7,4 +7,4 @@ current_dir = os.getcwd()
 
 COINBASE_API_KEY = os.getenv("COINBASE_API_KEY")
 COINBASE_API_SECRET = os.getenv("COINBASE_API_SECRET")
-COINBASE_15MIN_PATH = os.path.join(current_dir, "data\\DataPreprocessing\\datasets\\15min\\coinbase_15min.csv")
+COINBASE_15MIN_PATH = os.path.join(current_dir, "data\\datasets\\15min\\coinbase_15min.csv")

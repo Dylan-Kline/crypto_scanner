58eddab3d29a310b6c8deeecf84eeb07efada4a2
Kline <klinedylan46@gmail.com>
2024-10-02 02:39:45 -0400

    refactor: 002 remove incorrectly trained models, still an issue of predicting the same class only

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-from src.models.nn_algo_paper_model import NN_Algo
-from src.models.train import train_model
+from torch_lr_finder import LRFinder
+from torch import nn
+import torch.optim as optim
+
+from src.models.nn_algo_paper_model import NN_Algo, NN_algo_model
+from src.models.train import train_model, _load_data
 from src.data_processing.Extract_Crypto_Data import fetch_historical_crypto_data
 from src.data_processing.FeatureExtraction import extract_features_OHLCV, remove_columns_processed_data
 from src.data_processing.Label import label_crypto_AB
@@ -20,6 +24,7 @@ def main():
     until = int(pd.Timestamp(f"{until_date}", tz='America/New_York').timestamp() * 1000)
     fetch = False
     process = False
+    find_lr = False
     remove_cols = False
     backtest_since_date = '2022-01-01'
     backtest_until_date = '2022-04-01'
@@ -32,11 +37,28 @@ def main():
     
     bWin = 5
     fWin = 1
-    training_pipeline_OHLCV(raw_data=data, candle_interval='15min', backward_window=bWin, forward_window=fWin)
+    if process:
+        training_pipeline_OHLCV(raw_data=data, candle_interval='15min', backward_window=bWin, forward_window=fWin)
     train_path = CRYPTO_15MIN_PATH + f"scaled_labeled_bWin_{bWin}_fWin_{fWin}_train.csv"
     val_path = CRYPTO_15MIN_PATH + f"scaled_labeled_bWin_{bWin}_fWin_{fWin}_val.csv"
-    model = NN_Algo(200, 5)
-    train_model(model, train_path=train_path, val_path=val_path)
+    
+    if find_lr:
+        model = NN_algo_model(200, 3)
+    else:
+        model = NN_Algo(200, 3)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=0.0001)
+    
+    # find optimal learning rate
+    if find_lr:
+        train_loader, val_loader = _load_data(train_path, val_path)
+        lr_finder = LRFinder(model, optimizer, criterion, device='cuda:0')
+        lr_finder.range_test(train_loader, val_loader)
+        lr_finder.plot()
+        lr_finder.reset()
+        
+    else:
+        train_model(model, train_path=train_path, val_path=val_path)
     
 main()
 
--- src/models/models_config.py
+++ src/models/models_config.py
@@ -2,4 +2,4 @@ import os
 
 current_dir = os.getcwd()
 
-MLP_MODEL_CHECKPOINT_PATH = os.path.join(current_dir, "src/models/checkpoints/best_model_epoch=19_val_loss=0.87.ckpt")
+MLP_MODEL_CHECKPOINT_PATH = os.path.join(current_dir, "src/models/checkpoints/best_model_epoch=03_val_loss=0.87.ckpt")
